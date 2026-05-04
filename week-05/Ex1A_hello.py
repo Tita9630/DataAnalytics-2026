a29a549bdf0573ae6dc8f9ce64f1d0ef923f97dc
Tita9630 <hailutita@gmail.com>
2026-05-04 15:15:08 -0400

    Practicing Python

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,17 @@
 print('Hello world!')
+
+message = 'Hello world!'
+print(message)
+# "Hello world!" prints twice because of the print(Hello world!) on the top as well.
+
+
+# Displaying dollars and cents
+dollars = 3
+cents = .50
+print(dollars + cents)
+cents = cents + .25
+# I still have my two "Hello world!" result and the addition of valrable 'dollars' and 'cents'.
+print(dollars + cents)
+d_str = '3 dollars'
+c_str = '50 cents'
+print(d_str + " " + c_str)
