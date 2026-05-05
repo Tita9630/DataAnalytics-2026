8248fff527c03c46ac7f39801c7defe0a4e7dddb
Tita9630 <hailutita@gmail.com>
2026-05-05 15:11:01 -0400

    Updated_Ex2A_MathScripts

---
+++ week-05/Ex2A_MathScripts/net_worth.py
@@ -0,0 +1,7 @@
+total_asset = 1000000
+total_debt = 300200
+net_worth = total_asset - total_debt
+
+print(f"Your total assets are {total_asset}")
+print(f"Your total debts are {total_debt}")
+print(f"Your net worth is {net_worth}")
