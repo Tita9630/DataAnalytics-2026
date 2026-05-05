cc788a9f85d6e3c8925477615d832080e37e4143
Tita9630 <hailutita@gmail.com>
2026-05-04 22:39:27 -0400

    Updated_EX1B_variable_examples.py

---
+++ week-05/Ex1B_variable_examples.py
@@ -0,0 +1,24 @@
+customer_id = 1 # I used number because it can be eaisly generated as a unique value.
+customer_name = 'Vanessa Alex' # First name and Last name as a customer name.
+customer_gender = 'Female' #That's the customer's gender.
+customer_date_of_birth = '03/17/1997' #Here even though, date of birth is number I used '' because I has a unique character.
+driver_license_number = 'e748399212' # Again, it is a combination of numbers and a word so I used ''.
+auto_policy_number = 7382923319 # It is a collection of number. 
+
+
+name = 'Tihitina Zergaw'
+city ='Addis Ababa'
+country = 'Ethiopia'
+
+# list of reserved words that can’t be used for variable names
+# False	def	if	raise None	del	import	return True	elif in	try
+# and else is while as except lambda with assert finally nonlocal yield
+# break	for	not class form or continue global pass
+
+
+# False -- it represents the opposite of True. eg: Tihitina == tihitina
+# def -- is used to create a function so we can reuse code instead of repeating it.
+#if -- it used to make a conditional statement and if the condition is true, do something.
+# raise -- it is used to stop a program and show error when something goes wrong.
+# None -- it is similar to null(no value)
+
