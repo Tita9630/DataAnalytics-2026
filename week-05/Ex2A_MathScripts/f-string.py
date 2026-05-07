870553e9441e1b64e35af6389854356e74994e55
Tita9630 <hailutita@gmail.com>
2026-05-06 21:57:25 -0400

    Added f-string.py

---
+++ week-05/Ex2A_MathScripts/f-string.py
@@ -0,0 +1,13 @@
+d_str = '3 dollars'
+c_str = '50 cents'
+print(f"{d_str + c_str}")
+
+
+food_cost = 79.25 
+tax = 6.54 
+tip = 12.00
+
+total_due = food_cost + tax + tip 
+
+print(f"Food cost is {food_cost} and tax is {tax}")
+print(f"Total due is {total_due}")
