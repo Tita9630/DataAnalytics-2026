068e67d24ff578b5e89746c700e56ce0454420bb
Tita9630 <hailutita@gmail.com>
2026-05-09 16:24:23 -0400

    Added min_max.py

---
+++ week-05/Ex4B_MoreIfScripts/min_max.py
@@ -0,0 +1,17 @@
+a = 15500
+b = 1200
+c =100500
+
+if a < b and a < c:
+    print("smallest:", a)
+elif b < a and b < c:
+    print("smalles:", b)
+else:
+    print("smallest:", c) 
+
+if a > b and a > c:
+    print("largest:", a)
+elif b > a and b > c:
+    print("largest:", b)
+else:
+    print("largest:", c) 
