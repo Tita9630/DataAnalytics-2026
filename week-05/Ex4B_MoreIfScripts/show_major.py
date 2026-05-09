ac74533c3d11b6b93b7ca8ba7c96c30814e3829e
Tita9630 <hailutita@gmail.com>
2026-05-09 17:41:14 -0400

    Added show_major.py

---
+++ week-05/Ex4B_MoreIfScripts/show_major.py
@@ -0,0 +1,20 @@
+student_name = "Luna"
+student_major = "DATA"
+
+if student_major == "BIOL":
+    print("Biology")
+    print("Science Bldg, Room 310")
+elif student_major == "CSCI":
+    print("Computer Science")
+    print("Sheppard Hall, Room 314")
+elif student_major == "ENG":
+    print("English")
+    print("Kerr Hall, Room 201")
+elif student_major == "HIST":
+    print("History")
+    print("Kerr Hall, Room 114")
+elif student_major == "MKT":
+    print("Marketing")
+    print("Westly Hall, Room 310")
+else:
+    print("<unknown>")
