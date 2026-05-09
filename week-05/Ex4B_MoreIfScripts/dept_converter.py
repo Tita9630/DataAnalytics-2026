88f31836ebe1d588513da7572fed42c823500b25
Tita9630 <hailutita@gmail.com>
2026-05-09 14:48:58 -0400

    Added dept_converter.py

---
+++ week-05/Ex4B_MoreIfScripts/dept_converter.py
@@ -0,0 +1,15 @@
+department_code = 25
+if department_code == 1:
+    print("Marketing")
+elif department_code == 5:
+    print("Human Resources")
+elif department_code == 10:
+    print("Accounting")
+elif department_code == 12:
+    print("Legal")
+elif department_code == 18:
+    print("IT")
+elif department_code == 20:
+    print("Customer Relations")
+else:
+    print("Error")
