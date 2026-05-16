7e437896af719c383d167416fa15162fbc787993
Tita9630 <hailutita@gmail.com>
2026-05-16 09:30:12 -0400

    Added open_and_write.py

---
+++ week-06/Ex4A_ReadWriteFiles/open_and_write.py
@@ -0,0 +1,6 @@
+f = open("about_me.txt", "a")
+f.close()
+
+f = open("about_me.txt", "a")
+f.write("\nPerfect night out: A quite place under the moon with my favorite person and favorite meal.")
+f.close()
